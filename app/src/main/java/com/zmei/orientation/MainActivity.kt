9bad611919856790455569895e40c37aecbbfe04
arhizmei <arhizmei@gmail.com>
2023-02-24 16:29:33 +0200

    work5

--- app/src/main/java/com/zmei/orientation/MainActivity.kt
+++ app/src/main/java/com/zmei/orientation/MainActivity.kt
@@ -2,10 +2,26 @@ package com.zmei.orientation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.zmei.orientation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+    bindingClass.button1.setOnClickListener {
+
+        var editReader = bindingClass.editText1.text.toString()
+        bindingClass.text1.visibility = View.VISIBLE
+        when (editReader) {
+
+            "andrey" -> bindingClass.text1.text = "your name is andrey"
+            "vadim" -> bindingClass.text1.text = "your name is vadim" }
+
     }
-}
+    }
+
+    }
